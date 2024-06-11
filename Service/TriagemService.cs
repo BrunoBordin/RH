c6287b5aa441a635bb4c1a0380a6bbb4770043fe
Bruno Bordin <brunobordin70@gmail.com>
2024-06-10 22:58:32 -0300

    remove metodo faltante

--- Service/TriagemService.cs
+++ Service/TriagemService.cs
@@ -20,33 +20,7 @@ namespace RH.Service
 
         public async Task<List<VagaTecnologiaRequisitoDto>> GerarRelatorioCandidatosVaga(int idVaga)
         {
-            var vaga = await _repositoryVaga.BuscarVagaTecnologiasPorIdVaga(idVaga);
-
-            var candidatos = await _repositoryCandidato.BuscarCandidatosPorIdVaga(idVaga);
-
-            var candidatosPontuados = new List<VagaTecnologiaRequisitoDto>();
-
-            foreach (var candidato in candidatos)
-            {
-                int pontuacaoTotal = 0;
-
-                foreach (var ct in candidato)
-                {
-                    var vagaTecnologia = vaga.FirstOrDefault(vt => vt.IdTecnologia == ct.IdTecnologia);
-                    if (vagaTecnologia != null)
-                    {
-                        pontuacaoTotal += vagaTecnologia.Peso;
-                    }
-                }
-                var entidade = _mapper.Map<CandidatoDto>(candidato);
-                candidatosPontuados.Add(new CandidatoPontuadoDto
-                {
-                    CandidatoDto = entidade,
-                    Pontuacao = pontuacaoTotal
-                });
-            }
-
-            return candidatosPontuados.OrderByDescending(cp => cp.Peso).ToList();
+            return null;
         }
     }
 }
